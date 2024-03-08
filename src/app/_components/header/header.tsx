68d619793227c6d595d98c99d4d93542111825c7
BrunoRRamos <bruno.ramos@ccc.ufcg.edu.br>
2024-03-08 18:04:28 -0300

    style(header): Estilizando header

--- src/app/_components/header/header.tsx
+++ src/app/_components/header/header.tsx
@@ -1,20 +1,21 @@
-import { ButtonHeader } from "../componentsSys/headerButton";
-
 export function Header() {
   return (
-    <div className="flex flex-col h-screen w-2/4 bg-black items-center py-24 ">
-      <h1 className="text-white text-5xl font-bold">Bruno Ramos</h1>
-      <h2 className="text-white">Full Stack developer</h2>
+    <div className="flex flex-col h-screen w-2/4 bg-black items-start py-24 ">
+      <h1 className="text-slate-200 text-5xl font-bold">Bruno Ramos</h1>
+      <h2 className="text-slate-200 mt-1.5">Full Stack developer</h2>
+      <p className="text-slate-300 leading-normal max-w-xs mt-4">
+        Responsive, Interactive, Eficient
+      </p>
 
-      <nav className="mt-16">
-        <ul className="[&>li]:text-white">
+      <nav>
+        <ul className="[&>li]:text-white mt-16 w-max h-min">
           <li>ABOUT</li>
           <li>EXPERIENCE</li>
           <li>PROJECTS</li>
         </ul>
       </nav>
 
-      <ul className="[&>li]:text-white">
+      <ul className="[&>li]:text-white mt-16">
         <li>GitHub</li>
         <li>LinkedIn</li>
         <li>Email</li>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Header } from "./_components/header/header";
 
 export default function main() {
   return (
-    <div className="bg-white">
+    <div className="px-24 bg-slate-900">
       <Header></Header>
     </div>
   );
